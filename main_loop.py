d98e9973f22733a2fd30da5c301f96d175871213
Erik Ribeiro <erik@ribeiro.com>
2024-07-13 03:04:56 +0100

    adicionando timer

--- main_loop.py
+++ main_loop.py
@@ -1,5 +1,4 @@
 import time
-
 from config import API_KEY, API_SECRET, SYMBOL, QUANTITY, INTERVAL, SETUP
 from data_interface import LiveData
 from strategy import TradingStrategy
@@ -31,22 +30,31 @@ def main_loop():
     is_buy = False
     trade_history = read_trade_history()
 
+    last_log_time = time.time()  # Inicializa o temporizador de logs
+
     while True:
         try:
+            current_time = time.time()
             is_buy, trade_history = check_last_transaction(data_interface, SYMBOL)
             metrics.loop_counter_metric.labels(SYMBOL).inc()
 
             if is_buy and not is_comprado_logged:
-                logger.info("Bot v2 iniciado - Loop de venda.")
+                if current_time - last_log_time >= 30:
+                    logger.info("Bot v2 iniciado - Loop de venda.")
+                    last_log_time = current_time
                 is_comprado_logged = True
                 is_not_comprado_logged = False
 
             if not is_buy and not is_not_comprado_logged:
-                logger.info("Bot v2 iniciado - Loop de compra.")
+                if current_time - last_log_time >= 30:
+                    logger.info("Bot v2 iniciado - Loop de compra.")
+                    last_log_time = current_time
                 is_not_comprado_logged = True
 
             if is_buy:
-                logger.info("Executando lógica de venda...")
+                if current_time - last_log_time >= 30:
+                    logger.info("Executando lógica de venda...")
+                    last_log_time = current_time
                 is_buy, trade_history = strategy.sell_logic(
                     trade_history['valor_compra'].iloc[-1],
                     trade_history,
@@ -54,7 +62,9 @@ def main_loop():
                     trade_history['max_referencia'].iloc[-1]
                 )
             else:
-                logger.info("Executando lógica de compra...")
+                if current_time - last_log_time >= 30:
+                    logger.info("Executando lógica de compra...")
+                    last_log_time = current_time
                 is_buy, trade_history = strategy.buy_logic(
                     trade_history['valor_compra'].ewm(span=9, adjust=False).mean().iloc[-2],
                     trade_history['valor_compra'].ewm(span=9, adjust=False).mean().iloc[-3],
@@ -67,7 +77,6 @@ def main_loop():
         except Exception as e:
             logger.error(f"Erro inesperado: {e}")
             time.sleep(25)
-
 
 if __name__ == "__main__":
     main_loop()
