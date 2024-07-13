12382c1174926be4125ba43d83968be3d617861b
Erik Ribeiro <erik@ribeiro.com>
2024-07-13 03:08:58 +0100

    correção no deploy anterior

--- strategy.py
+++ strategy.py
@@ -1,12 +1,9 @@
-import pandas as pd
 import time
-
-from utils import calculate_percentage, logger
 from datetime import datetime
+from utils import calculate_percentage, logger
+import pandas as pd
 
 class TradingStrategy:
-
-
     def __init__(self, data_interface, metrics, symbol, quantity, interval, setup):
         self.data_interface = data_interface
         self.metrics = metrics
@@ -15,7 +12,7 @@ class TradingStrategy:
         self.interval = interval
         self.setup = setup
         self.position_maintained = False
-
+        self.last_log_time = time.time()  # Inicializa o temporizador de logs
 
     def evaluate(self, is_buy, trade_history):
         data = self.data_interface.get_historical_data(self.symbol, self.interval)
@@ -28,35 +25,34 @@ class TradingStrategy:
         previous_high = data['high'].iloc[-2]
         previous_low = data['low'].iloc[-2]
 
+        current_time = time.time()
         if is_buy:
-            self.sell_logic(current_price, trade_history, previous_low, previous_high)
+            if current_time - self.last_log_time >= 30:
+                logger.info("Executando lógica de venda...")
+                self.last_log_time = current_time
+            is_buy, trade_history = self.sell_logic(current_price, trade_history, previous_low, previous_high)
         else:
+            if current_time - self.last_log_time >= 30:
+                logger.info("Executando lógica de compra...")
+                self.last_log_time = current_time
             is_buy, trade_history = self.buy_logic(previous_ema, pre_previous_ema, current_price, previous_high, previous_low, trade_history)
 
         return is_buy, trade_history
-
 
     def sell_logic(self, current_price, trade_history, previous_low, previous_high):
         if not self.position_maintained:
             logger.info("Loop de venda - Checando condições de venda.")
             self.position_maintained = True
 
-        logger.info("Obtendo preço atual...")
         ticker = self.data_interface.get_current_price(self.symbol)
         if ticker is None:
-            logger.warning("Preço atual não obtido. Tentando novamente...")
             return True, trade_history
 
-        logger.info(f"Preço atual obtido: {ticker}")
-        
         stoploss = trade_history['stoploss'].iloc[-1]
         stopgain = trade_history['stopgain'].iloc[-1]
         mid_stoploss = previous_low
 
-        logger.info(f"Condições de venda - Stoploss: {stoploss}, Stopgain: {stopgain}, Mid Stoploss: {mid_stoploss}")
-
         if ticker <= stoploss or ticker >= stopgain or (ticker <= mid_stoploss and mid_stoploss > trade_history['valor_compra'].iloc[-1]):
-            logger.info("Condições de venda atendidas, tentando executar venda...")
             start_time = time.time()
             balance_btc = self.data_interface.get_current_balance('BTC')
             lot_size = self.data_interface.get_lot_size(self.symbol)
@@ -85,10 +81,7 @@ class TradingStrategy:
                 self.position_maintained = False
                 return False, trade_history
 
-        logger.info("Condições de venda não atendidas, mantendo posição.")
         return True, trade_history  # Continua indicando que está comprado
-
-
 
     def buy_logic(self, previous_ema, pre_previous_ema, current_price, previous_high, previous_low, trade_history):
         if not self.position_maintained:
@@ -128,21 +121,3 @@ class TradingStrategy:
             return True, trade_history
 
         return False, trade_history
-
-
-    def update_metrics_on_sell(self, ticker):
-        self.metrics.last_sell_price_metric.labels(self.symbol).set(ticker)
-        self.metrics.successful_sells_metric.labels(self.symbol).inc()
-        self.metrics.sell_price_spread_metric.labels(self.symbol).set(max(self.metrics.sell_prices) - min(self.metrics.sell_prices) if self.metrics.sell_prices else 0)
-
-
-    def update_metrics_on_buy(self, current_price, stoploss, stopgain, mid_stoploss, potential_loss, potential_gain):
-        self.metrics.current_stoploss_metric.labels(self.symbol).set(stoploss)
-        self.metrics.current_stopgain_metric.labels(self.symbol).set(stopgain)
-        self.metrics.last_buy_price_metric.labels(self.symbol).set(current_price)
-        self.metrics.buy_attempts_metric.labels(self.symbol).inc()
-        self.metrics.successful_buys_metric.labels(self.symbol).inc()
-        self.metrics.buy_price_spread_metric.labels(self.symbol).set(max(self.metrics.buy_prices) - min(self.metrics.buy_prices) if self.metrics.buy_prices else 0)
-        self.metrics.potential_loss_metric.labels(self.symbol).set(potential_loss)
-        self.metrics.potential_gain_metric.labels(self.symbol).set(potential_gain)
-        self.metrics.mid_stoploss_metric.labels(self.symbol).set(mid_stoploss)
