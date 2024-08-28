b25b74984dd73f980a5eaff6d0ff3bedc9dd0bc7
Erik Ribeiro <erik@ribeiro.com>
2024-08-28 18:10:05 +0100

    debug2

--- strategy.py
+++ strategy.py
@@ -50,8 +50,9 @@ class TradingStrategy:
 
         if current_time - self.last_log_time >= 2:
             logger.info(f"Condições de venda - Stoploss: {stoploss}, Stopgain: {stopgain}")
+            print('COMEÇO')
             print(data.iloc[-1])
-            print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
+            print('FIM')
             self.last_log_time = current_time
 
         if sell_stoploss(data['low'].iloc[-1], stoploss) or sell_stopgain(data['high'].iloc[-1], stopgain):
