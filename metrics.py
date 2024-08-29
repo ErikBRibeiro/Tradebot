56657741b54dcb4568fc415da226c17f5e358dd2
Bruno Castro Dias da Fonseca <b.castro.doute@gmail.com>
2024-08-29 02:43:10 -0300

    Feat: server timeout metrics

--- metrics.py
+++ metrics.py
@@ -1,10 +1,9 @@
 from prometheus_client import CollectorRegistry, Gauge, Counter, Histogram, Summary, start_http_server
 
+registry = CollectorRegistry()
+
 class Metrics:
     def __init__(self, currency):
-        registry = CollectorRegistry()
-
-        # Métricas Prometheus
         self.current_price_metric = Gauge('trade_bot_current_price', 'Current price of the asset', ['currency'], registry=registry)
         self.current_trade_volume_metric = Gauge('trade_bot_current_trade_volume', 'Current volume of the asset traded', ['currency'], registry=registry)
         self.total_trades_count_metric = Gauge('trade_bot_total_trades_count', 'Total count of trades', ['currency'], registry=registry)
@@ -18,8 +17,6 @@ class Metrics:
         self.current_high_price_metric = Gauge('trade_bot_current_high_price', 'Current high price of the asset', ['currency'], registry=registry)
         self.current_low_price_metric = Gauge('trade_bot_current_low_price', 'Current low price of the asset', ['currency'], registry=registry)
         self.current_volume_metric = Gauge('trade_bot_current_volume', 'Current trading volume', ['currency'], registry=registry)
-
-        # Atualizado ao comprar
         self.buy_attempts_metric = Counter('trade_bot_buy_attempts', 'Number of buy attempts', ['currency'], registry=registry)
         self.successful_buys_metric = Counter('trade_bot_successful_buys', 'Number of successful buys', ['currency'], registry=registry)
         self.current_stoploss_metric = Gauge('trade_bot_current_stoploss', 'Current stoploss value', ['currency'], registry=registry)
@@ -29,15 +26,11 @@ class Metrics:
         self.buy_price_spread_metric = Gauge('trade_bot_buy_price_spread', 'Spread between buy prices', ['currency'], registry=registry)
         self.potential_loss_metric = Gauge('trade_bot_potential_loss', 'Potential loss of current buy', ['currency'], registry=registry)
         self.potential_gain_metric = Gauge('trade_bot_potential_gain', 'Potential gain of current buy', ['currency'], registry=registry)
-
-        # Atualizado ao vender
         self.successful_sells_metric = Counter('trade_bot_successful_sells', 'Number of successful sells', ['currency'], registry=registry)
         self.last_sell_price_metric = Gauge('trade_bot_last_sell_price', 'Last sell price', ['currency'], registry=registry)
         self.sell_duration_metric = Histogram('trade_bot_sell_duration_seconds', 'Duration of sell transactions in seconds', ['currency'], registry=registry)
         self.transaction_outcome_metric = Summary('trade_bot_transaction_outcome', 'Transaction outcomes (gain/loss)', ['currency'], registry=registry)
         self.sell_price_spread_metric = Gauge('trade_bot_sell_price_spread', 'Spread between sell prices', ['currency'], registry=registry)
-
-        # Atualizado após cada trade
         self.total_trades_metric = Counter('trade_bot_total_trades', 'Total number of trades', ['currency'], registry=registry)
         self.total_loss_metric = Gauge('trade_bot_total_loss', 'Total loss accumulated', ['currency'], registry=registry)
         self.trade_volume_metric = Gauge('trade_bot_trade_volume', 'Total volume of trades', ['currency'], registry=registry)
@@ -45,11 +38,10 @@ class Metrics:
         self.total_profit_metric = Gauge('trade_bot_total_profit', 'Total profit accumulated', ['currency'], registry=registry)
         self.average_trade_duration_metric = Gauge('trade_bot_average_trade_duration', 'Average duration of trades', ['currency'], registry=registry)
         self.profit_factor_metric = Gauge('trade_bot_profit_factor', 'Profit factor (total profit / total loss)', ['currency'], registry=registry)
-
-        # Contador para o número de vezes que o loop principal é executado
+        self.server_status_metric = Gauge('trade_bot_server_status', 'Status of the trade bot server (1 = up, 0 = down)', registry=registry)
+        self.server_down_metric_duration = Histogram('trade_bot_down_duration', 'Offline duration', registry=registry)
         self.loop_counter_metric = Counter('trade_bot_loop_executions', 'Number of loop executions', ['currency'], registry=registry)
 
-        # Variáveis adicionais
         self.total_trade_duration = 0
         self.buy_prices = []
         self.sell_prices = []
