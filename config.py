d7ccf4f9b05e141a748d471f18fec8b0ae9bfa53
Erik Ribeiro <erik@ribeiro.com>
2024-09-11 10:42:19 +0100

    removing unused content

--- config.py
+++ config.py
@@ -9,8 +9,3 @@ API_SECRET = os.getenv('BYBIT_API_SECRET')
 
 if not API_KEY or not API_SECRET:
     raise ValueError("API Key ou API Secret não encontrada. Verifique o arquivo .env.")
-
-SYMBOL = "BTCUSDT"
-QUANTITY = 0.0011  
-INTERVAL = '15m'  
-SETUP = "9.1"  
